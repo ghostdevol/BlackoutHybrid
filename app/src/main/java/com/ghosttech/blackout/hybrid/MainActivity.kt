92a84b1d8e34804a6d623a59c7183d2b39ac208b
Ghostdevol <karmastudiosbeatz@gmail.com>
2026-03-01 06:11:29 -0600

    Update MainActivity.kt

--- app/src/main/java/com/ghosttech/blackout/hybrid/MainActivity.kt
+++ app/src/main/java/com/ghosttech/blackout/hybrid/MainActivity.kt
@@ -1,25 +1,19 @@
+package com.ghosttech.blackouthybrid
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var webView: WebView
-    private lateinit var wallet: WalletContext
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        wallet = WalletContext(this)
-
         setContentView(R.layout.activity_main)
 
-        webView = findViewById(R.id.webview)
-
+        val webView = findViewById<WebView>(R.id.webview)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
-
-        webView.addJavascriptInterface(BridgeInterface(wallet), "Blackout")
-
         webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
 
         webView.loadUrl("file:///android_asset/index.html")
     }
